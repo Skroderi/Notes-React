ec0458a82cb4191ac091b84e865c2c34e53881be
Skroderi <bakaminski@wp.pl>
2019-07-04 10:48:24 +0200

    add redux

---
+++ src/reducers/index.js
@@ -0,0 +1,62 @@
+const initialState = {
+  twitters: [
+    {
+      id: 1,
+      title: 'Hello World',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+      twitterName: 'Arsenal',
+    },
+    {
+      id: 2,
+      title: 'Redux guy',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+      twitterName: 'dan_abramov',
+    },
+    {
+      id: 3,
+      title: 'React router stuff',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+      twitterName: 'mjackson',
+    },
+    {
+      id: 4,
+      title: 'Super animacje!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+      twitterName: 'sarah_edo',
+    },
+  ],
+
+  acrticles: [
+    {
+      id: 1,
+      title: 'React on my mind',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/',
+      created: '1 day',
+    },
+  ],
+  notes: [
+    {
+      id: 1,
+      title: 'Wake me up when Vue ends',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+  ],
+};
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default rootReducer;
---
+++ src/views/Twitters.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import GridTemplate from 'templates/GridTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const Twitters = ({ twitters }) => (
+  <GridTemplate pageType="twitters">
+    {twitters.map(({ title, content, twitterName, created, id }) => (
+      <Card
+        id={id}
+        cardType="twitters"
+        title={title}
+        content={content}
+        twitterName={twitterName}
+        created={created}
+        key={id}
+      />
+    ))}
+  </GridTemplate>
+);
+
+const mapStateToProps = state => {
+  const { twitters } = state;
+  return { twitters };
+};
+// export default Twitters;
+export default connect(mapStateToProps)(Twitters);
